1d3412216aa8fb0d58dfc19ab2d521dbbecb12c6
udonwajnr <udonwajnr10@gmail.com>
2023-05-21 22:32:32 +0100

    commit

--- src/components/TrendingMovies.jsx
+++ src/components/TrendingMovies.jsx
@@ -12,8 +12,8 @@ export default function Trending({toggle}){
         setToggleState(index);
     };
     return (
-        <section className="mt-20 px-8 lg:px-2">
-            <div className="flex items-center gap-x-2">
+        <section className="mt-10 px-8 lg:px-2">
+            <div className="flex justify-start lg:justify-between items-center gap-x-5">
                 <h1 className="text-white text-3xl" onClick={toggle}>Trending</h1>
                 <MovieTab toggleTab={toggleTab} toggleState={toggleState}/>
             </div>
