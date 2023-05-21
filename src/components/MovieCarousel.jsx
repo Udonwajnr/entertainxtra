36d635109f9bfbd52f403c0b79cb09622b001194
udonwajnr <udonwajnr10@gmail.com>
2023-05-22 00:25:06 +0100

    commit

--- src/components/MovieCarousel.jsx
+++ src/components/MovieCarousel.jsx
@@ -31,18 +31,18 @@ export default function MovieCarousel(){
                     <div className='text-white max-w-xl px-8 lg:px-2'>
                         <span className='bg-yellow-500 text-2xl font-bold p-1 rounded'>New</span>
                         <h1 className='text-7xl font-bold mt-5 lg:text-2xl'>StarWars</h1>
-                        <div className='text-white grid grid-cols-3  gap-x-2 mt-5 max-w-[150px]'>
-                            <span className="lg:text-xs text-sm text-center">2021</span>
-                            <span className='border rounded text-sm lg:text-xs text-center'>+18</span>
-                            <span className="lg:text-xs text-sm text-center">2h 6m</span>
+                        <div className='text-white flex space-x-3 mt-5'>
+                            <span className="lg:text-xs text-sm">2021</span>
+                            <span className='border rounded text-sm lg:text-xs' >+18</span>
+                            <span className="lg:text-xs text-sm">2h 6m</span>
                         </div>
                         <p className='mt-5'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, reprehenderit at architecto ducimus harum neque recusandae itaque eaque totam sapiente expedita voluptates ipsam ipsum ad doloremque fuga dicta enim. Tempore!</p>
-                        <div className='grid grid-cols-2 max-w-[300px] gap-x-4 lg:gap-x-10 mt-5'>
-                            <Link href={'/'} className='py-2 px-3 flex rounded-md bg-red gap-x-2 justify-center items-center text-xl md:text-sm'>
+                        <div className='flex space-x-4 gap-x-4 mt-5'>
+                            <Link href={'/description'} className='py-2 px-3 flex rounded-md bg-red gap-x-2 justify-between items-center text-xl'>
                             {/* <FaPlay size={25}/> */}
-                             Play Now
+                             Details
                              </Link>
-                            <Link href={'/'} className=' py-2 px-3 flex rounded-md bg-red gap-x-2 justify-center items-center text-xl bg md:text-sm' >
+                            <Link href={'/'} className=' py-2 px-3 flex rounded-md bg-red gap-x-2 items-center text-xl ' >
                             {/* <BiCameraMovie size={25}/> */}
                              Trailer
                             </Link>
@@ -56,24 +56,24 @@ export default function MovieCarousel(){
                     <div className='hero-img absolute w-full h-screen -z-10'>
                         <img src="https://c4.wallpaperflare.com/wallpaper/381/863/742/star-wars-star-wars-the-last-jedi-lightsaber-rey-from-star-wars-wallpaper-preview.jpg" alt="" className=' w-full h-screen object-cover'/> 
                     </div>
-                    <div className='text-white max-w-xl px-8 lg:px-2'>
+                    <div className='  text-white max-w-xl px-8 lg:px-2'>
                         <span className='bg-yellow-500 text-2xl font-bold p-1 rounded'>New</span>
-                        <h1 className='text-7xl font-bold mt-5 lg:text-2xl'>StarWars</h1>
-                        <div className='text-white flex justify-start items-center  gap-x-3 mt-5'>
-                            <span className="lg:text-xs text-sm">2021</span>
-                            <span className='border rounded text-sm lg:text-xs px-1'>+18</span>
-                            <span className="lg:text-xs text-sm">2h 6m</span>
+                        <h1 className='text-7xl font-bold mt-5'>StarWars</h1>
+                        <div className='text-white flex gap-x-3 mt-5'>
+                            <span>2021</span>
+                            <span className='border rounded border-red-500 px-1'>+18</span>
+                            <span>2h 6m</span>
                         </div>
                         <p className='mt-5'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, reprehenderit at architecto ducimus harum neque recusandae itaque eaque totam sapiente expedita voluptates ipsam ipsum ad doloremque fuga dicta enim. Tempore!</p>
-                        <div className='grid grid-cols-2 max-w-[300px] gap-x-4 lg:gap-x-10 mt-5'>
-                            <Link href={'/'} className='py-2 px-3 flex rounded-md bg-red gap-x-2 justify-center items-center text-xl md:text-sm'>
-                            {/* <FaPlay size={25}/> */}
-                             Play Now
-                             </Link>
-                            <Link href={'/'} className=' py-2 px-3 flex rounded-md bg-red gap-x-2 justify-center items-center text-xl md:text-sm'>
-                            {/* <BiCameraMovie size={25}/> */}
-                             Trailer
-                            </Link>
+                        <div className='flex space-x-4 lg:gap-x-10 mt-5'>
+                            <Link href={'/description'} className='py-2 px-3 flex rounded-md bg-red gap-x-2 items-center text-xl p-2'>
+                                 {/* <FaPlay size={25}/> */}
+                                Details
+                                </Link>
+                                <Link href={'/'} className='py-2 px-3 p-2 flex rounded-md bg-red gap-x-2 items-center text-xl bg'>
+                                    {/* <BiCameraMovie size={25}/> */}
+                                    Trailer
+                                </Link>
                         </div>
                     </div>
                 </div>
@@ -84,23 +84,23 @@ export default function MovieCarousel(){
                     <div className='hero-img absolute w-full h-screen -z-10'>
                         <img src="https://e0.pxfuel.com/wallpapers/837/406/desktop-wallpaper-luxury-mansion-exotic-houses.jpg" alt="" className='w-full h-screen object-cover'/> 
                     </div>
-                       <div className='text-white max-w-xl px-8 lg:px-2'>
+                    <div className='  text-white max-w-xl px-8 lg:px-2'>
                         <span className='bg-yellow-500 text-2xl font-bold p-1 rounded'>New</span>
-                        <h1 className='text-7xl font-bold mt-5 lg:text-2xl'>StarWars</h1>
-                        <div className='text-white grid grid-cols-3  gap-x-2 mt-5 max-w-[150px]'>
-                            <span className="lg:text-xs text-sm text-center">2021</span>
-                            <span className='border rounded text-sm lg:text-xs text-center'>+18</span>
-                            <span className="lg:text-xs text-sm text-center">2h 6m</span>
+                        <h1 className='text-7xl font-bold mt-5'>StarWars</h1>
+                        <div className='text-white flex gap-x-3 mt-5'>
+                            <span>2021</span>
+                            <span className='border rounded border-red-500 px-1'>+18</span>
+                            <span>2h 6m</span>
                         </div>
                         <p className='mt-5'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, reprehenderit at architecto ducimus harum neque recusandae itaque eaque totam sapiente expedita voluptates ipsam ipsum ad doloremque fuga dicta enim. Tempore!</p>
-                        <div className='grid grid-cols-2 max-w-[300px] gap-x-4 lg:gap-x-10 mt-5'>
-                            <Link href={'/'} className='py-2 px-3 flex rounded-md bg-red gap-x-2 justify-center items-center text-xl md:text-sm'>
-                            {/* <FaPlay size={25}/> */}
-                             Play Now
-                             </Link>
-                            <Link href={'/'} className=' py-2 px-3 flex rounded-md bg-red gap-x-2 justify-center items-center text-xl md:text-sm' >
-                            {/* <BiCameraMovie size={25}/> */}
-                             Trailer
+                        <div className='flex space-x-4 mt-5'>
+                            <Link href={'/description'} className='py-2 px-3 flex rounded-md bg-red gap-x-2 items-center text-xl'>
+                                 {/* <FaPlay size={25}/> */}
+                                  Detail
+                            </Link>
+                            <Link href={'/'} className='py-2 px-3 flex rounded-md bg-red gap-x-2 items-center text-xl bg' >
+                                {/* <BiCameraMovie size={25}/> */}
+                                 Trailer
                             </Link>
                         </div>
                     </div>
