729fcac3caffe8e9b2bfd1b21211403aec3c1d15
udonwajnr <udonwajnr10@gmail.com>
2023-05-21 20:51:00 +0100

    commit

--- src/components/RelatedMovies.jsx
+++ src/components/RelatedMovies.jsx
@@ -3,22 +3,21 @@ import SeriesCard from "./SeriesCard"
 export default function RelatedMovies(){
     return(
     <section className="py-5">
-    <div className=' mt-8 px-10'>
-        <div className='mt-4'>
-            <h2 className='text-3xl text-white'>Related Movies</h2>
+        <div className=' mt-8 px-10 lg:px-2'>
+            <div className='mt-4'>
+                <h2 className='text-3xl text-white'>Related Movies</h2>
+            </div>
+            <div className="grid grid-cols-fluid gap-4 md:grid-cols-2 mt-5">
+                <MovieCard/>
+                <MovieCard/>
+                <MovieCard/>
+                <SeriesCard/>
+                <SeriesCard/>
+                <SeriesCard/>
+                <SeriesCard/>
+                <SeriesCard/>
+            </div>
         </div>
-        <div className="grid grid-cols-fluid gap-4 sm:grid-cols-2 mt-5">
-            <MovieCard/>
-            <MovieCard/>
-            <MovieCard/>
-            <SeriesCard/>
-            <SeriesCard/>
-            <SeriesCard/>
-            <SeriesCard/>
-            <SeriesCard/>
-        </div>
-    </div>
-
     </section>
     )
 }
