7bf40426c409eb5d942e63ff09bff587dc24448f
udonwajnr <udonwajnr10@gmail.com>
2023-05-21 22:17:58 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
+     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -39,6 +40,7 @@ module.exports = {
 
     },
   },
+
   plugins: [
   ],
 }
