70cd41ed04377da2425d4498f8c81ea446da14f0
udonwajnr <udonwajnr10@gmail.com>
2023-05-21 22:34:23 +0100

    commit

--- src/components/MovieTabs.jsx
+++ src/components/MovieTabs.jsx
@@ -3,7 +3,7 @@ import {BsPlayCircle,BsListUl} from 'react-icons/bs'
 export default function MovieTab({toggleTab, toggleState}){
     return (
         <>
-            <div className="flex gap-x-2">
+            <div className="flex lg:justify-between gap-x-3">
                 <button
                 className={toggleState === 1 ? "tabs flex items-center justify-center gap-x-1 active-tabs bg-red p-2 text-white" : "tabs  p-2 flex items-center gap-x-1 bg-white"}
                 onClick={() => toggleTab(1)}
